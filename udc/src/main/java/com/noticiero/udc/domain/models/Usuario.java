ea99622d860a870f43550254fced0944ff96ede5
aperezm15 <aperezm15@unicartagena.edu.co>
2026-05-27 12:21:44 -0500

    feat(Dominio.models): Creando modelo Usuario.

---
+++ udc/src/main/java/com/noticiero/udc/domain/models/Usuario.java
@@ -0,0 +1,39 @@
+package com.noticiero.udc.domain.models;
+
+import com.noticiero.udc.domain.enums.UserRole;
+import com.noticiero.udc.domain.enums.UserStatus;
+import com.noticiero.udc.domain.valueobjects.UserEmail;
+
+public class Usuario {
+    private Long id;
+    private String nombre;
+    private UserEmail email;
+    private String password;
+    private UserRole role;
+    private UserStatus status;
+
+    public Usuario(Long id, String nombre, UserEmail email, String password, UserRole role, UserStatus status) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
+        setRole(role);
+        setStatus(status);
+    }
+
+    public void setRole(UserRole role) {
+        if (role != null) {
+            this.role = role;
+        } else {
+            this.role = UserRole.LECTOR;
+        }
+    }
+
+    public void setStatus(UserStatus status) {
+        if (status != null) {
+            this.status = status;
+        } else {
+            this.status = UserStatus.INACTIVO;
+        }
+    }
+}
