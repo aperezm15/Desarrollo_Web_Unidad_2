a96ae88b40e234e790375d609dbb5b7dafdf570a
aperezm15 <aperezm15@unicartagena.edu.co>
2026-05-28 14:39:59 -0500

    feat(Application.ports.out): Creando UserRepository para traer y guardar informacion a la base de datos de la tabla encargada del modelo Usuario.

---
+++ udc/src/main/java/com/noticiero/udc/application/ports/out/UserRepositoryPort.java
@@ -0,0 +1,15 @@
+package com.noticiero.udc.application.ports.out;
+
+import com.noticiero.udc.domain.models.Usuario;
+import com.noticiero.udc.domain.valueobjects.UserEmail;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepositoryPort {
+    Usuario save (Usuario usuario);
+    Optional<Usuario> finById (Long id);
+    Optional<Usuario> finByEmail (UserEmail email);
+    List<Usuario> findAll ();
+    void deleteById (Long id);
+}
