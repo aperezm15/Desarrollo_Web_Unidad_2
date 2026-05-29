088a9cfe53784597badcbfc61e25f0f69d234330
aperezm15 <aperezm15@unicartagena.edu.co>
2026-05-29 13:35:12 -0500

    feat(Infrastructure.adapters.persistence): Creando UserEntity para mapear modelo de negocio.

---
+++ udc/src/main/java/com/noticiero/udc/infrastructure/adapters/persistence/UserEntity.java
@@ -0,0 +1,91 @@
+package com.noticiero.udc.infrastructure.adapters.persistence;
+
+
+import com.noticiero.udc.domain.enums.UserRole;
+import com.noticiero.udc.domain.enums.UserStatus;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String nombre;
+
+    @Column(nullable = false, unique = true, length = 180)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private UserRole role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private UserStatus status;
+
+    public UserEntity() {}
+
+    public UserEntity(Long id, String nombre, String email, String password, UserRole role, UserStatus status) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public UserStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(UserStatus status) {
+        this.status = status;
+    }
+}
