07bbd8e5000f795de47575af7fa5e53ccd8be2a5
aperezm15 <aperezm15@unicartagena.edu.co>
2026-05-30 21:41:07 -0500

    feat(infracstructure.adapters.persistence): Creando JPA para Noticia.

---
+++ udc/src/main/java/com/noticiero/udc/infrastructure/adapters/persistence/SpringDataNoticiaRepository.java
@@ -0,0 +1,9 @@
+package com.noticiero.udc.infrastructure.adapters.persistence;
+
+import com.noticiero.udc.domain.models.Noticia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SpringDataNoticiaRepository extends JpaRepository<Noticia, Long> {
+}
