a537d9a1fba5de678401bc61a9a2efc22e6a2157
aperezm15 <aperezm15@unicartagena.edu.co>
2026-05-31 00:15:46 -0500

    feat(infracstructure.adapters.config):

---
+++ udc/src/main/java/com/noticiero/udc/infrastructure/config/NoticiaConfig.java
@@ -0,0 +1,14 @@
+package com.noticiero.udc.infrastructure.config;
+
+import com.noticiero.udc.application.ports.in.NoticiaUseCase;
+import com.noticiero.udc.application.ports.out.NoticiaRepositoryPort;
+import com.noticiero.udc.application.services.NoticiaService;
+import org.springframework.context.annotation.Bean;
+
+public class NoticiaConfig {
+
+    @Bean
+    public NoticiaUseCase noticiaService(NoticiaRepositoryPort noticiaRepositoryPort) {
+        return new NoticiaService(noticiaRepositoryPort);
+    }
+}
