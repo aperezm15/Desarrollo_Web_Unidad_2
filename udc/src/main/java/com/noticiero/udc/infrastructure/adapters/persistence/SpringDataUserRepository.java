871ea60230cedda84523d4c23ef7d00403a301e2
aperezm15 <aperezm15@unicartagena.edu.co>
2026-06-01 05:06:41 -0500

    feat(vistas): agregando if en vistas para dar opciones dependiendo el rol

--- udc/src/main/java/com/noticiero/udc/infrastructure/adapters/persistence/SpringDataUserRepository.java
+++ udc/src/main/java/com/noticiero/udc/infrastructure/adapters/persistence/SpringDataUserRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 public interface SpringDataUserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
     Optional<UserEntity> findByVerificationToken(String token);
+
 }
